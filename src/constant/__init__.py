715c25aed1fdb8747117cf1e0a285b88af602abf
AJAY <aashu221301@gmail.com>
2025-10-07 09:21:36 +0530

    completed all module

---
+++ src/constant/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+AWS_S3_BUCKET_NAME="wafer-fault"
+MONGO_DATABASE_NAME="ASkills"
+MONGO_COLLECTION_NAME="waferfault"
+
+TARGET_COLUMN="quality"
+MONGO_DB_URL="mongodb+srv://aashu221301_db_user:U21MVdJkaqXvO6Dn@cluster0.2ovymbd.mongodb.net/?retryWrites=true&w=majority"
+
+MODEL_FILE_NAME="model"
+MODEL_FILE_EXTENSION=".pkl"
+
+artifact_folder = "artifacts"
