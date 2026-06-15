91e11a2962a8da8948badd2292b5a35583471bee
AJAY <aashu221301@gmail.com>
2026-06-15 15:27:12 +0530

    chnaged model_trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -92,53 +92,6 @@ class ModelTrainer:
             raise CustomException(e, sys)
 
 
-
-
-    def get_best_model(self,
-                    x_train:np.array,
-                    y_train: np.array,
-                    x_test:np.array,
-                    y_test: np.array):
-        try:
-           
-             
-
-
-            model_report: dict = self.evaluate_models(
-                 x_train =  x_train,
-                 y_train = y_train,
-                 x_test =  x_test,
-                 y_test = y_test,
-                 models = self.models
-            )
-
-
-            print(model_report)
-
-
-            best_model_score = max(sorted(model_report.values()))
-
-
-            ## To get best model name from dict
-
-
-            best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]
-
-
-            best_model_object = self.models[best_model_name]
-
-
-
-
-            return best_model_name, best_model_object, best_model_score
-
-
-
-
-        except Exception as e:
-            raise CustomException(e,sys)
        
     def finetune_best_model(self,
                             best_model_object:object,
@@ -179,12 +132,6 @@ class ModelTrainer:
 
 
 
-
-
-
-
-
-
     def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info(f"Splitting training and testing input and target feature")
@@ -202,12 +149,6 @@ class ModelTrainer:
 
 
             logging.info(f"Extracting model config file path")
-
-
-
-
-           
-
 
 
 
@@ -282,10 +223,5 @@ class ModelTrainer:
 
 
            
-
-
-           
-
-
         except Exception as e:
             raise CustomException(e, sys)
